d7b0f9cf066a7d0e69a03d4fbfd9a28c4bb4496e
Borjuto <borja.antona@gmail.com>
2025-01-23 21:17:06 +0100

    (feat): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,22 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'shop',
+    loadComponent: () => import('./shop/shop.page').then((m) => m.ShopPage),
+  },
+  {
+    path: 'product',
+    loadComponent: () => import('./product/product.page').then((m) => m.ProductPage),
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./cart/cart.page').then((m) => m.CartPage),
+  },
+  {
+    path: 'category',
+    loadComponent: () => import('./category/category.page').then((m) => m.CategoryPage),
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
