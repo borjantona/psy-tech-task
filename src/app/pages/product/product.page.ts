780b2ded72bd17b6efa0fa00921db04a5ee0c43c
Borjuto <borja.antona@gmail.com>
2025-01-24 16:56:38 +0100

    (feat): add product page

--- src/app/pages/product/product.page.ts
+++ src/app/pages/product/product.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
+import { IonContent, IonButton } from '@ionic/angular/standalone';
+import { Category, Product } from 'src/app/interfaces/product';
+import { ApiFetcherService } from 'src/app/services/apiFetcher';
 
 @Component({
   selector: 'app-product',
   templateUrl: 'product.page.html',
   styleUrls: ['product.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonContent, IonButton],
 })
 export class ProductPage {
-  constructor() {}
+  productId: number;
+  product: Product;
+  constructor(private apiFetcherService: ApiFetcherService, private route: ActivatedRoute) {}
+  
+	ngOnInit() {
+	  this.productId = this.route.snapshot.params['id'];
+	  this.loadProduct();
+	}
+
+  addToCart() {
+	console.log('Product added to cart');
+  }
+
+  loadProduct() {
+	this.apiFetcherService.getProduct(this.productId).then((product: Product) => { this.product = product; });
+  }
 }
