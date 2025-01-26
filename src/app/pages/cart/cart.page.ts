2cf332cb944fa8c0d6c3ae5a8ec5ebecc767730b
Borjuto <borja.antona@gmail.com>
2025-01-26 21:08:55 +0100

    (fix): style empty cart

--- src/app/pages/cart/cart.page.ts
+++ src/app/pages/cart/cart.page.ts
@@ -1,4 +1,4 @@
-import { DecimalPipe } from '@angular/common';
+import { DecimalPipe, NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
 import { Store } from '@ngrx/store';
@@ -16,7 +16,7 @@ import { ProductCardComponent } from "../../components/product-card/product-card
   selector: 'app-cart',
   templateUrl: 'cart.page.html',
   styleUrls: ['cart.page.scss'],
-  imports: [IonContent, ProductCheckoutCardComponent, DecimalPipe, StyledButtonComponent, ProductCardComponent],
+  imports: [IonContent, ProductCheckoutCardComponent, NgClass, DecimalPipe, StyledButtonComponent, ProductCardComponent],
 })
 export class CartPage implements OnInit {
   cart$: Observable<Cart>;
@@ -32,21 +32,24 @@ export class CartPage implements OnInit {
     this.cart$ = this.store.select(selectCart);
     this.cart$.subscribe((cart) => {
       this.cart = cart;
-	  if (this.products) {
-		this.totalPrice = this.cart.products.reduce((total, cartProd) => {
-			const prod = this.products.find((p) => p.id === cartProd.productId);
-			if (prod) {
-				total += prod.price*cartProd.quantity;
-			}
-			return total;
-		}, 0)
-	  }
+	  this.updatePrice();
     });
     this.products$ = this.store.select(selectProductsStore);
     this.products$.subscribe((productsStore) => {
       this.products = productsStore.products;
       this.favourites = productsStore.favourites;
+	  this.updatePrice();
     });
+  }
+
+  updatePrice() {
+    this.totalPrice = this.cart.products.reduce((total, cartProd) => {
+      const prod = this.products.find((p) => p.id === cartProd.productId);
+      if (prod) {
+        total += prod.price * cartProd.quantity;
+      }
+      return total;
+    }, 0);
   }
   
 }
