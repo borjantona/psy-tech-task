14d71cceafeeed2c3be804b71962fe1c9c6b0c64
Borjuto <borja.antona@gmail.com>
2025-01-26 12:51:51 +0100

    (fix): fix phone validation regex

--- src/app/components/elements/styled-input/styled-input.component.ts
+++ src/app/components/elements/styled-input/styled-input.component.ts
@@ -158,7 +158,7 @@ export class StyledInputComponent implements OnChanges {
 export const REGEX = {
   MAIL: /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
   NIF: /([a-z]|[A-Z]|[0-9])[0-9]{7}([a-z]|[A-Z]|[0-9])/,
-  PHONE: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]{5,}$/,
+  PHONE: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]{7,}$/,
   NUMBER: /^[0-9]*$/,
   NUMBER_WITH_DECIMALS: /^[+-]?((\d+(\.\d*)?)|(\.\d+))$/,
   PASSWORD:
