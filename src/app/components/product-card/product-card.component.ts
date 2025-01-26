a6eaa96918232f4662c82a65819ece0b89a162ec
Borjuto <borja.antona@gmail.com>
2025-01-26 18:14:18 +0100

    (feat): add fav and add to cart buttons in product card

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,21 +1,26 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { IonIcon } from '@ionic/angular/standalone'
 
 import { Product } from 'src/app/interfaces/product';
 import { addProduct } from 'src/app/store/cart/cart.actions';
+import { addIcons } from 'ionicons';
+import { cartOutline, heartOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'product-card',
   templateUrl: 'product-card.component.html',
   styleUrls: ['product-card.component.scss'],
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, IonIcon],
 })
 export class ProductCardComponent {
   @Input() product: Product;
 
-  constructor(private router: Router, private store: Store) {}
+  constructor(private router: Router, private store: Store) {
+		addIcons({ cartOutline, heartOutline });
+  }
 
 
   goToProductDetails() {
@@ -24,5 +29,9 @@ export class ProductCardComponent {
 
   addProductToCart() {
 	this.store.dispatch(addProduct({ product: this.product }));
+  }
+
+  likeProduct() {
+	
   }
 }
